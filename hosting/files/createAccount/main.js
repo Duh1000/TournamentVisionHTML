f1eabd140f8eab69fa7336a96ae0f9772362dd08
duh1000 <duh1000000@gmail.com>
2022-01-11 12:24:03 -0600

    add signup messages

--- hosting/files/createAccount/main.js
+++ hosting/files/createAccount/main.js
@@ -3,7 +3,29 @@ const app = new Realm.App({ id: "tournamentvisionwebsite-kkmpt" });
 async function createAccount(email, password) {
     console.log(email);
     console.log(password);
-    await app.emailPasswordAuth.registerUser({ email, password });
+    try{
+      let userRead = "";
+
+      await app.emailPasswordAuth.registerUser({ email, password });
+      //add a promise return
+
+      userRead = "Created successfully";
+    } catch(error) {
+      console.log("already exists");
+      console.log(error.statusCode);
+
+      switch(error.statusCode) {
+        case 400:
+          userRead = "Password must be at least 8 characters";
+          break;
+        case 409:
+          userRead = "User already exists";
+          break;
+        default:
+          userRead = "Sorry, an unexpected error has occurred. Make sure your username is an email"
+      }
+    }
+    document.getElementById("error").innerHTML = userRead;
 }
 
 document.getElementById('signupbtn').onclick = function() {
